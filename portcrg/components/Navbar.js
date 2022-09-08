ec408bf0e3d2a33bc39041c02c659c03801afb79
Pablo Garzona <prgarzona@gmail.com>
2022-09-08 10:06:22 -0600

    Redireccion a /profile

--- portcrg/components/Navbar.js
+++ portcrg/components/Navbar.js
@@ -51,7 +51,7 @@ const Navbar = () => {
         </div>
         <div>
           <a
-            href="#"
+            href="/profile"
             className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-cherry-red hover:bg-white mt-4 sm:mt-0"
           >
             Usuario
