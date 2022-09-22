3e7bb4cf5e95adbe9b711ba188e9fee90426541b
GeovannyTS <38725404+geovannysalvador@users.noreply.github.com>
2022-09-22 02:16:55 -0600

    enviar datos a la api

--- portcrg/components/FormCourses.jsx
+++ portcrg/components/FormCourses.jsx
@@ -1,45 +1,87 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import axios from "axios";
-export default function FormCourses({ setOpen, setAlert, product }) {
-  const formRef = useRef(null);
 
+export default function FormCourses() {
   const [name, setName] = useState("");
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
   const [description, setDescription] = useState("");
   const [duration, setDuration] = useState("");
   const [diploma, setDiploma] = useState("");
-  const [instructor, setInstructor] = useState("")
+  const [instructor, setInstructor] = useState("");
   const [modality, setModality] = useState("");
   const [amountOfParticipants, setAmountOfParticipants] = useState("");
   const [status, setStatus] = useState("");
-  const [sede, setSede] = useState("");
+  const [photographyURL, setPhotographyURL] = useState("");
+  // const [sede, setSede] = useState("");
   const [platform, setPlatform] = useState("");
-  const [prerequisitos, setPrerequisitos] = useState("");
-
+  // const [prerequisitos, setPrerequisitos] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    submitCourses()
+    submitCourses();
+  };
+
+
+  const insertarImagen = (e) => {
+    setPhotographyURL(e)
+    const f = new FormData();
+
+    for (let index = 0; index < photographyURL.length; index++){
+      f.append("files", photographyURL[index])
+    }
+    setPhotographyURL(f);
   };
 
   const submitCourses = async () => {
     try {
-   
+      // if ([name, instructor].includes("")) {
+      //   alert("Todos los campos son obligatorios");
+      //   return;
+      // }
 
-      console.log(name, startDate, endDate, description, duration, diploma, instructor,
-        modality, amountOfParticipants, status, platform)
+      const { data } = await axios.post(
+        "https://portcrg-dev.onrender.com/api/courses/",
+        {
+          name,
+          startDate,
+          endDate,
+          description,
+          duration,
+          diploma,
+          instructor,
+          modality,
+          amountOfParticipants,
+          status,
+          photographyURL,
+          platform,
+        }
+      );
 
- 
-      
+      console.log(
+        name,
+        startDate,
+        endDate,
+        description,
+        duration,
+        diploma,
+        instructor,
+        modality,
+        amountOfParticipants,
+        status,
+        platform,
+        photographyURL
+      );
+
+      console.log(data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   return (
-    <form ref={formRef} onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit}>
       <div className="overflow-hidden">
         <div className="px-4 py-5 bg-white sm:p-6">
           <div className="grid grid-cols-6 gap-6">
@@ -52,7 +94,6 @@ export default function FormCourses({ setOpen, setAlert, product }) {
               </label>
               <input
                 type="text"
-                name="name"
                 id="name"
                 className="border-2 border-rose-200 w-full p-2 mt-2 placeholder-red-400 rounded-md"
                 value={name}
@@ -69,7 +110,6 @@ export default function FormCourses({ setOpen, setAlert, product }) {
               </label>
               <input
                 type="number"
-                name="title"
                 id="duracion"
                 className="border-2 border-rose-200 w-full p-2 mt-2 placeholder-red-400 rounded-md"
                 value={duration}
@@ -78,16 +118,12 @@ export default function FormCourses({ setOpen, setAlert, product }) {
             </div>
 
             <div className="col-span-6 sm:col-span-3">
-              <label
-                htmlFor="price"
-                className="block text-sm font-medium "
-              >
+              <label htmlFor="price" className="block text-sm font-medium ">
                 Fecha de Inicio
               </label>
               <input
                 type="date"
-                name="price"
-                id="price"
+                id="fecha"
                 className="border-2 border-rose-200 w-full p-2 mt-2 placeholder-red-400 rounded-md"
                 value={startDate}
                 onChange={(e) => setStartDate(e.target.value)}
@@ -96,15 +132,14 @@ export default function FormCourses({ setOpen, setAlert, product }) {
 
             <div className="col-span-6 sm:col-span-3">
               <label
-                htmlFor="price"
+                htmlFor="fecha-finalizacion"
                 className="block text-sm font-medium text-gray-700"
               >
                 Fecha de Finalizacion
               </label>
               <input
                 type="date"
-                name="price"
-                id="price"
+                id="endStart"
                 className="border-2 border-rose-200 w-full p-2 mt-2 placeholder-red-400 rounded-md"
                 value={endDate}
                 onChange={(e) => setEndDate(e.target.value)}
@@ -113,48 +148,35 @@ export default function FormCourses({ setOpen, setAlert, product }) {
 
             <div className="col-span-6 sm:col-span-3">
               <label
-                htmlFor="title"
+                htmlFor="Habilitar"
                 className="block text-sm font-medium placeholder-red-400"
               >
                 Habilitar Diploma
               </label>
 
-              <div className="grid grid-cols-2 ">
-                <label className=" px-16">SI</label>
-                <input
-                  type="radio"
-                  name="title"
-                  id="title"
-                  className="border-2 border-rose-300 w-full mx-0 placeholder-red-400 rounded-md"
-                  value={diploma}
-                  onChange={(e) => setDiploma(e.target.value)}
-                />
-              </div>
-
-              <div className="grid  border-rose-200 grid-cols-2 mt-4">
-                <label className=" px-16">No</label>
-                <input
-                  type="radio"
-                  name="title"
-                  id="title"
-                  className="border-2 border-rose-300 w-full mx-0 placeholder-red-400 rounded-md"
-                  value={diploma}
-                  onChange={(e) => setDiploma(e.target.value)}
-                />
-              </div>
+              <select
+                id="Habilitar-diploma"
+                value={diploma}
+                onChange={(e) => setDiploma(e.target.value)}
+                className="mt-1 block w-full border-rose-300 py-2 px-3 border bg-white rounded-md shadow-sm focus:outline-none focus:placeholder-red-400 focus:border-placeholder-red-400 sm:text-sm text-black"
+              >
+                <option>-- Seleccionar --</option>
+                <option value={true}>SI</option>
+                <option value={false}>NO</option>
+              </select>
             </div>
 
             <div className="col-span-6 sm:col-span-3">
               <label
-                htmlFor="title"
+                htmlFor="Nombre-instructor"
                 className="block text-sm font-medium placeholder-red-400"
               >
                 Nombre del Instructor
               </label>
               <input
                 type="text"
-                name="title"
-                id="title"
+                name="Nombre"
+                id="nameInstructor"
                 className="border-2  border-rose-200 w-full p-2 mt-2 placeholder-red-400 rounded-md"
                 value={instructor}
                 onChange={(e) => setInstructor(e.target.value)}
@@ -163,61 +185,59 @@ export default function FormCourses({ setOpen, setAlert, product }) {
 
             <div className="col-span-6 sm:col-span-3">
               <label
-                htmlFor="title"
+                htmlFor="Numero-participantes"
                 className="block text-sm font-medium placeholder-red-400"
               >
                 Numero de participantes
               </label>
               <input
-                defaultValue={product?.title}
                 type="number"
-                name="title"
-                id="title"
+                id="Numero-participantes"
                 className="border-2 border-rose-200 w-full p-2 mt-2 placeholder-red-400rounded-md"
                 value={amountOfParticipants}
                 onChange={(e) => setAmountOfParticipants(e.target.value)}
               />
             </div>
 
-            
             <div className="col-span-6 sm:col-span-3">
               <label
-                htmlFor="category"
+                htmlFor="Modalidad"
                 className="block text-sm font-medium placeholder-red-400"
               >
                 Modalidad
               </label>
               <select
-                id="category"
-                name="category"
-                autoComplete="category-name"
+                id="Modalidad-id"
                 value={modality}
                 onChange={(e) => setModality(e.target.value)}
                 className="mt-1 block w-full border-rose-300 py-2 px-3 border bg-white rounded-md shadow-sm focus:outline-none focus:placeholder-red-400 focus:border-placeholder-red-400 sm:text-sm text-black"
               >
-                <option value="1" >Presencial</option>
+                <option>-- Seleccionar --</option>
                 <option value="1">Virtual</option>
+                <option value="2">Presencial</option>
+                <option value="3">Hibrido</option>
               </select>
             </div>
 
             <div className="col-span-6 sm:col-span-3">
               <label
-                htmlFor="category"
+                htmlFor="plataforma"
                 className="block text-sm font-medium placeholder-red-400"
               >
                 Plataforma
               </label>
               <select
-                id="category"
-                name="category"
+                id="plataforma"
                 value={platform}
                 onChange={(e) => setPlatform(e.target.value)}
-                autoComplete="category-name"
                 className="mt-1 block w-full py-2 px-3 border border-rose-300 bg-white rounded-md shadow-sm focus:outline-none focus:placeholder-red-400 focus:border-placeholder-red-400 sm:text-sm text-black"
               >
-                <option value="1">Ninguno</option>
-                <option value="1">Meet</option>
-                <option value="1">Zoom</option>
+                <option>-- Seleccionar --</option>
+                <option>Ninguno</option>
+                <option value="Meet">Meet</option>
+                <option value="Zoom">Zoom</option>
+                <option value="Teams">Teams</option>
+                <option value="Google Duo">Google Duo</option>
               </select>
             </div>
 
@@ -236,10 +256,11 @@ export default function FormCourses({ setOpen, setAlert, product }) {
                 autoComplete="category-name"
                 className="mt-1 block w-full py-2 px-3 border border-rose-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm text-black"
               >
+                <option>-- Seleccionar --</option>
                 <option value="1">Cancelado</option>
-                <option value="1">Habilitado</option>
-                <option value="1">Reprogramado</option>
-                <option value="1">Terminado</option>
+                <option value="2">Habilitado</option>
+                <option value="3">Reprogramado</option>
+                <option value="4">Terminado</option>
               </select>
             </div>
 
@@ -250,9 +271,9 @@ export default function FormCourses({ setOpen, setAlert, product }) {
               >
                 Descripcion
               </label>
-              <textarea               
-                name="title"
-                id="title"
+              <textarea
+                name="descripcion"
+                id="Descripcion"
                 className="border-2 border-rose-200 w-full p-2 mt-2 placeholder-red-400 rounded-md"
                 value={description}
                 onChange={(e) => setDescription(e.target.value)}
@@ -287,11 +308,11 @@ export default function FormCourses({ setOpen, setAlert, product }) {
                       >
                         <span>Actualizar Imagen</span>
                         <input
-                          defaultValue={product?.images}
                           id="images"
-                          name="images"
+                     
                           type="file"
                           className="sr-only"
+                          onChange={(e) => insertarImagen(e.target.files[0])}
                         />
                       </label>
                       <p className="pl-1">Cargar Imagen</p>
