c461a58dc1490afcb58230d9594a3b205e8276e4
GeovannyTS <38725404+geovannysalvador@users.noreply.github.com>
2022-09-22 03:07:49 -0600

    Fix card

--- portcrg/components/CardCourses.jsx
+++ portcrg/components/CardCourses.jsx
@@ -5,9 +5,9 @@ const CardCourses = ({ _id, name, description, photographyURL }) => {
     // photographyURL = "https://res.cloudinary.com/dzdoi1gfv/image/upload/v1663377085/PortCRG/byf2sfbs3csrww7jxfdf.jpg"
   return (
     <section className="mx-4 ">
-      <div className="flex flex-col justify-center items-center bg-slate-500 rounded-md mb-8">
+      <div className="flex flex-col justify-center items-center bg-slate-100 rounded-md mb-8 mt-24">
         <div className=" rounded-2xl">
-          <Image src={photographyURL} alt="" className="rounded-2xl" width={250} height={200} />
+          <Image src={photographyURL} alt="" className="rounded-2xl" width={300} height={200} />
         </div>
         <p className="font-bold spa my-2">{name}</p>
 
