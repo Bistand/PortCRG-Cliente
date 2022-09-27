c447c864b26ea0a7cfa9fe452e580d283349cf4f
Alejndro22 <38710085+Alejndro22@users.noreply.github.com>
2022-09-27 06:27:31 -0600

    Fix en button

--- portcrg/components/CourseGenInfo.js
+++ portcrg/components/CourseGenInfo.js
@@ -81,23 +81,11 @@ function CourseGenInfo(props) {
             // </Link>
             <button
               type="button"
-              className=" text-sm px-5 py-3 w-full md:w-auto rounded-lg uppercase font-bold
-              bg-cherry-red text-white text-center mt-5 flex gap-2 items-center justify-center"
+              className=" text-sm px-3 py-2 w-full md:w-auto rounded-lg uppercase font-bold
+              bg-dark-cadet-blue text-white text-center mt-2 flex gap-2 items-center justify-center"
               onClick={() => setModal(true)}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              Agregar Nuevo Curso
+              Ver temario
             </button>
           ) : (
             <p>Aún no se ha cargado el recurso</p>
