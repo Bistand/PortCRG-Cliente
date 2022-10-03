8ca59a19ce43b3493cc26b84864ac351d689e137
cesar_zapet <cesarzapet@hotmail.com>
2022-10-03 09:16:38 -0600

    alerta popup

--- portcrg/pages/sign_up.jsx
+++ portcrg/pages/sign_up.jsx
@@ -7,6 +7,8 @@ import axios from "axios"
 import { bodyStreamToNodeStream } from "next/dist/server/body-streams"
 import react from "react"
 import { useForm } from "react-hook-form"
+import Document, { Html, Main, NextScript } from "next/document";
+import MyDocument from "./_document"
 
 
 export default function sign_up() {
@@ -16,44 +18,45 @@ export default function sign_up() {
     const [number1, setnumber1] = useState('')
     const [email, setemail] = useState('')
     const [password, setpassword] = useState('')
-    const {register, handleSubmit, errors, reset}= useForm();
-    function recargar(){
+    const { register, handleSubmit, errors, reset } = useForm();
+    
+
+
+    function onSubmitForm(values) {
+        console.log(values);
+    }
+
+    const submitUsuario = async () => {
+        try {
+            const response = await fetch('https://portcrg-dev.onrender.com/api/user/register/admin', {
+                method: 'POST',
+                body: JSON.stringify({ fullName, dpi, occupation, number1, email, password }),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+            const data = await response.json()
+            console.log(data),alert("usuario creado")
+        } catch (error) {
+            console.log("no conecta sevidor")
+        }
         location.href= location.href
     }
 
-    function onSubmitForm(values) {console.log(values);
-    }
-
-    const submitUsuario = async () =>{
-        try {
-                    const response = await fetch('https://portcrg-dev.onrender.com/api/user/register/admin',{
-        method: 'POST',
-        body: JSON.stringify({fullName,dpi,occupation,number1,email,password}),
-        headers:{
-            'Content-Type': 'application/json',
-        },
-        })
-        const data = await response.json()
-        console.log(data), alert("se agrego correctamente"),location.href= location.href
-        } catch (error) {
-            console.log("no conecta sevidor"), alert("no se pudo crear el ususario")
-        }
-
-    }
-
     return (
-    
+
         <form id="form" class="form" onSubmit={handleSubmit(onSubmitForm)}>
-        <div className={styles.contenedor}>
-            <div className={styles.divTitulo}>
-                <h1 className={styles.htitulo1}>Registrar Usuario</h1>
-            </div>
-            <div className={styles.divCampos}>
-            <div className={styles.divespacio}>
-            </div>
+
+            <div className={styles.contenedor}>
+                <div className={styles.divTitulo}>
+                    <h1 className={styles.htitulo1}>Registrar Usuario</h1>
+                </div>
+                <div className={styles.divCampos}>
+                    <div className={styles.divespacio}>
+                    </div>
                     <div className={styles.divInputSeleccionar}>
-                    <h2  className={styles.htitulo2}>Ocupación</h2>
-                        <select name="List1" id="sel" value={occupation} onChange={e =>setoccupation(e.target.value)} className={styles.select} required>
+                        <h2 className={styles.htitulo2}>Ocupación</h2>
+                        <select name="List1" id="sel" value={occupation} onChange={e => setoccupation(e.target.value)} className={styles.select} required>
                             <option value="" selected disabled > Seleccionar</option>
                             <option value="1">Voluntario geneneral</option>
                             <option value="2">Socorrista</option>
@@ -65,63 +68,60 @@ export default function sign_up() {
                         <span class="note"></span>
                     </div>
                     <div className={styles.divNumero}>
-                     
-                    </div>  
+
+                    </div>
                     <div>
-                    <h2  className={styles.htitulo2}>Numero</h2>
-                       <input id="txtnu" className={styles.input} type="text"  maxLength={8} value={number1} onChange={e =>setnumber1(e.target.value)} placeholder="Numero" required pattern="[3-7]{1}[0-9]{7}"  />
-                       <span class="note"></span>           
+                        <h2 className={styles.htitulo2}>Numero</h2>
+                        <input id="txtnu" className={styles.input} type="text" maxLength={8} value={number1} onChange={e => setnumber1(e.target.value)} placeholder="Numero" required pattern="[3-7]{1}[0-9]{7}" />
+                        <span class="note"></span>
                     </div>
                     <div className={styles.divNumeroDpi}>
 
-                    </div>  
+                    </div>
                     <div>
-                    <h2  className={styles.htitulo2}>DPI</h2>
-                    <input id="txtd" className={styles.input} type="text" maxLength={13} value={dpi} onChange={e =>setdpi(e.target.value)} placeholder="DPI" pattern="[1-9]{1}[0-9]{12}" required />
-                    <span class="note"></span> 
+                        <h2 className={styles.htitulo2}>DPI</h2>
+                        <input id="txtd" className={styles.input} type="text" maxLength={13} value={dpi} onChange={e => setdpi(e.target.value)} placeholder="DPI- (CUI)" pattern="[1-9]{1}[0-9]{12}" required />
+                        <span class="note"></span>
                     </div>
                     <div className={styles.divNombre}>
-                        
+
                     </div>
                     <div>
-                    <h2  className={styles.htitulo2}>Nombre Completo</h2>
-                    <input id="txtn" className={styles.input} type="text" value={fullName} onChange={e =>setfullName(e.target.value)} placeholder="Nombre Completo" required pattern="^[a-zA-Z\s]{8,60}" />
-                    <span class="note"></span> 
+                        <h2 className={styles.htitulo2}>Nombre Completo</h2>
+                        <input id="txtn" className={styles.input} type="text" value={fullName} onChange={e => setfullName(e.target.value)} placeholder="Nombre Completo" required pattern="^[a-zA-Z\s]{8,60}" />
+                        <span class="note"></span>
                     </div>
                     <div className={styles.divEmail}>
-                      
+
                     </div>
                     <div className={styles.divInputEmail}>
-                    <h2  className={styles.htitulo2}>Email</h2>
-                        <input id="txte" className={styles.input} type="email" value={email} onChange={e =>setemail(e.target.value)} placeholder="Ejemplo@gmail.com" required/>
-                        <span class="note"></span> 
+                        <h2 className={styles.htitulo2}>Email</h2>
+                        <input id="txte" className={styles.input} type="email" value={email} onChange={e => setemail(e.target.value)} placeholder="Ejemplo@gmail.com" required />
+                        <span class="note"></span>
                     </div>
                     <div className={styles.divContraseña}>
-                    
+
                     </div>
-                    
-                    <h2  className={styles.htitulo2}>Contraseña</h2>
+
+                    <h2 className={styles.htitulo2}>Contraseña</h2>
                     <div className={styles.divInputConstaseña}>
-                        <input id="txtp" className={styles.input} type="text" value={password} onChange={e =>setpassword(e.target.value)} maxLength={8} placeholder="Contraseña" required/>
-                        <span class="note"></span> 
+                        <input id="txtp" className={styles.input} type="text" value={password} onChange={e => setpassword(e.target.value)} maxLength={8} placeholder="Contraseña" required />
+                        <span class="note"></span>
                     </div>
                     <div className={styles.divBoton}>
-                     <button id="btn" onClick={submitUsuario } className={styles.Boton}   >
+                        <button id="btn" onClick={submitUsuario} className={styles.Boton}   >
                             Ingresar
-                    </button >
+                        </button >
                     </div>
                     <div className={styles.popup} id="popup">
-                    <h3  >Usuario registrado Gracias</h3>
-                    <button id="btn2" onClick={recargar} className={styles.popupBoton}   >
+                        <h3  >Usuario registrado Gracias</h3>
+                        <button id="btn2" className={styles.popupBoton}   >
                             Aceptar
-                    </button >
+                        </button >
 
                     </div>
                 </div>
             </div>
-            <script>
-                
-            </script>
         </form>
 
     )
