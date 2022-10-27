df838c0ce55122b6cf230eaced5b51b0147e328e
cesar_zapet <cesarzapet@hotmail.com>
2022-10-27 08:23:25 -0600

    etiquetas_de_campos

--- portcrg/pages/sign_up.jsx
+++ portcrg/pages/sign_up.jsx
@@ -21,34 +21,34 @@ export default function sign_up() {
     const [confirmar, setconfirmar] = useState("")
     const [privileges, setprivilegios] = useState(0)
     const { register, handleSubmit, errors, reset } = useForm();
-    function recargarr(){
+    function recargarr() {
         location.href = location.href;
     }
-    const [superUsuario, setsuperUsuario]= useState(true);
+    const [superUsuario, setsuperUsuario] = useState(true);
     const tokenuser = getCookie("tokenuser");
     let perfil
-    const comparacionprivileges= async ()=> {
+    const comparacionprivileges = async () => {
         const usuario = await Getdatosuser(tokenuser);
         perfil = usuario.data
         console.log(perfil.privileges)
-        if(perfil.privileges === 1){
+        if (perfil.privileges === 1) {
             setsuperUsuario(false)
-        }else{
+        } else {
             setsuperUsuario(true)
         }
-        
+
     }
-    useEffect(()=>{
+    useEffect(() => {
         comparacionprivileges()
     })
-    
+
 
     function onSubmitForm(values) {
         console.log(values);
     }
     const submitUsuario = async () => {
         const usertoken = getCookie("tokenuser");
-        let confir= confirmar;
+        let confir = confirmar;
         let Fnomb = fullName;
         let Dpei = dpi;
         let ocu = occupation;
@@ -56,57 +56,58 @@ export default function sign_up() {
         let ema = email;
         let pss = password;
         let priv = privileges;
-        if ((Fnomb != "") & (Dpei >1000000000000) & (ocu != "") & (num1 >29999999) & (ema != "") & (pss != "") & (priv != 0)) {
-            if(pss==confir){
+        if ((Fnomb != "") & (Dpei > 1000000000000) & (ocu != "") & (num1 > 29999999) & (ema != "") & (pss != "") & (priv != 0)) {
+            if (pss == confir) {
                 swal('Procesando...'
-                 ,{buttons: ["finalizar"]});
-            const response = await fetch('https://portcrg-dev.onrender.com/api/user/register/admin', {
-                method: 'POST',
-                body: JSON.stringify({ fullName, dpi, occupation, number1, email, password,privileges }),
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: "Bearer " + usertoken,
-                },
-            })
-            const data = await response.json();
-            console.log(data);
-            if(data.code=400){ 
-                swal(data.response, 'Correo Registrado Previamente; ¡Ingrese otro!',
-                   
-                    'error',{buttons: ["finalizar"]});
-            }
-            if(data.code=200){ 
+                    , { buttons: ["finalizar"] });
+                const response = await fetch('https://portcrg-dev.onrender.com/api/user/register/admin', {
+                    method: 'POST',
+                    body: JSON.stringify({ fullName, dpi, occupation, number1, email, password, privileges }),
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: "Bearer " + usertoken,
+                    },
+                })
+                const data = await response.json();
+                console.log(data);
+                if (data.code = 400) {
+                    swal(data.response, 'Correo Registrado Previamente; ¡Ingrese otro!',
+
+                        'error', { buttons: ["finalizar"] });
+                }
+                if (data.code = 200) {
+                    swal('Cruz Roja Quetzaltenango',
+                        data.message,
+                        'success', { buttons: ["finalizar"] });
+                    setTimeout(() => {
+                        location.href = location.href;
+                    }, 3000);
+                }
+            } else {
                 swal('Cruz Roja Quetzaltenango',
-            data.message,
-            'success',{buttons: ["finalizar"]});
-            setTimeout(() => {
-                location.href = location.href;
-            }, 3000);}
-        }else {
-            swal('Cruz Roja Quetzaltenango',
-            'La contraseña no es igual',
-            'error',{buttons: ["finalizar"],timer: 2000 });
-        }
-            
+                    'La contraseña no es igual',
+                    'error', { buttons: ["finalizar"], timer: 2000 });
+            }
+
         } else {
             swal('Cruz Roja Quetzaltenango',
-            'LLene todos los campos correctamente',
-            'error',{buttons: ["finalizar"],timer: 3000 });
+                'LLene todos los campos correctamente',
+                'error', { buttons: ["finalizar"], timer: 3000 });
         }
     }
     return (
         <div className={styles.general}>
             <div className={styles.bod} >
-               <div className={styles.contenedor}>
+                <div className={styles.contenedor}>
 
-                <div className={styles.text}>Ingresar Usuario</div>
+                    <div className={styles.text}>Ingresar Usuario</div>
 
-            
-                <form id="form" class="form" onSubmit={handleSubmit(onSubmitForm)}>
-                <div className={styles.formrow}>
-                                <div className={styles.inputdata} >
-                           
-                                <select name="List1" id="sel" value={occupation} onChange={e => setoccupation(e.target.value)} className={styles.select} required>
+
+                    <form id="form" class="form" onSubmit={handleSubmit(onSubmitForm)}>
+                        <div className={styles.formrow}>
+                            <div className={styles.inputdata} >
+
+                                <select name="List1" id="sel" value={occupation} onChange={e => setoccupation(e.target.value)} className={styles.select} required >
                                     <option value="" selected disabled > Seleccionar Ocupación</option>
                                     <option value="1">Voluntario General</option>
                                     <option value="2">Socorrista</option>
@@ -116,93 +117,93 @@ export default function sign_up() {
                                     <option value="6">Administrador</option>
                                 </select>
                                 <div className={styles.underline}></div>
-                               
-                                </div>
 
-                                <div className={styles.inputdata} >
-                                <input id="txtnu"  type="text" maxLength={8} value={number1} onChange={e => setnumber1(e.target.value)} required pattern="[3-7]{1}[0-9]{7}" />
+                            </div>
+
+                            <div className={styles.inputdata} >
+                                <input id="txtnu" type="text" maxLength={8} value={number1} onChange={e => setnumber1(e.target.value)} required pattern="[3-7]{1}[0-9]{7}" title="Numero de 8 dijitos"/>
                                 <div className={styles.underline}></div>
                                 <label>Número Telefónico</label>
-                               
-                               
-                                </div>
 
 
-                           
-                </div>
-                <div className={styles.formrow}>
-                                <div className={styles.inputdata} >
-                                <input id="txtd"  type="text" maxLength={13} value={dpi} onChange={e => setdpi(e.target.value)} pattern="[1-9]{1}[0-9]{12}" required />
+                            </div>
+
+
+
+                        </div>
+                        <div className={styles.formrow}>
+                            <div className={styles.inputdata} >
+                                <input id="txtd" type="text" maxLength={13} value={dpi} onChange={e => setdpi(e.target.value)} pattern="[1-9]{1}[0-9]{12}" required title="Numero de 13 dijitos" />
                                 <div className={styles.underline}></div>
                                 <label >DPI o CUI</label>
-                                </div>
+                            </div>
 
-                               <div className={styles.inputdata} >
-                              
-                                <input id="txtn"  type="text" value={fullName} onChange={e => setfullName(e.target.value)}  required pattern="^[a-zA-ZÀ-ÿ\s]{10,60}" />
+                            <div className={styles.inputdata} >
+
+                                <input id="txtn" type="text" value={fullName} onChange={e => setfullName(e.target.value)} required pattern="^[a-zA-ZÀ-ÿ\s]{10,60}" title="Nombre solo letras"/>
                                 <div className={styles.underline}></div>
                                 <label>Nombre Completo</label>
-                               
-                                </div>
+
+                            </div>
 
 
 
-                </div>   
-                    <div className={styles.formrow}>  
-                                <div className={styles.inputdata} >
-                                
-                                <input id="txte"  type="email" value={email} onChange={e => setemail(e.target.value)} required />
+                        </div>
+                        <div className={styles.formrow}>
+                            <div className={styles.inputdata} >
+
+                                <input id="txte" type="email" value={email} onChange={e => setemail(e.target.value)} required title="Correo valido, correo unico por ususario" />
                                 <div className={styles.underline}></div>
-                                <label>Email</label>                               
-                                </div>
+                                <label>Email</label>
+                            </div>
 
 
 
-                                <div className={styles.inputdata} >
-                                <select name="List2" id="selp" onChange={e => setprivilegios(e.target.value) } className={styles.select} required>
+                            <div className={styles.inputdata} >
+                                <select name="List2" id="selp" onChange={e => setprivilegios(e.target.value)} className={styles.select} required>
                                     <option value="" selected disabled >Tipo de Privilegios</option>
                                     <option value={3}>Usuario</option>
                                     <option value={2} hidden={superUsuario} >Administrador</option>
                                 </select>
                                 <div className={styles.underline}></div>
-                                </div>
+                            </div>
 
-      
-                
-                    </div>     
-                    <div className={styles.formrow}> 
 
-                    <div className={styles.inputdata} >
-                                <input id="txtp"  type="password" value={password} onChange={e => setpassword(e.target.value)}  maxLength={15}  required pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)([A-Za-z\d]|[^ ]){8,15}$"/>
+
+                        </div>
+                        <div className={styles.formrow}>
+
+                            <div className={styles.inputdata} >
+                                <input id="txtp" type="password" value={password} onChange={e => setpassword(e.target.value)} maxLength={15} required pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)([A-Za-z\d]|[^ ]){8,15}$" title="Debe contener por lo menos un número y una letra mayúscula, 8 caracteres mínimo y 15 máximo"/>
                                 <div className={styles.underline}></div>
                                 <label>Contraseña</label>
-                                
-                                </div>
 
-                 
+                            </div>
+
+
 
 
 
                             <div className={styles.inputdata} >
-                                <input id="txtp"  type="password" value={confirmar} onChange={e => setconfirmar(e.target.value)} maxLength={15}  required pattern={password}/>
+                                <input id="txtp" type="password" value={confirmar} onChange={e => setconfirmar(e.target.value)} maxLength={15} required pattern={password} title="Debe coincidir con la contraseña" />
                                 <div className={styles.underline}></div>
                                 <label> Confirmar Contraseña</label>
-                                
-                                </div>
-                    </div>   
-                    <div className={styles.formrow}> 
-                    <div className={styles.inputdata}>
+
+                            </div>
+                        </div>
+                        <div className={styles.formrow}>
+                            <div className={styles.inputdata}>
                                 <button id="btn" onClick={submitUsuario} className={styles.Boton} >
                                     Ingresar
                                 </button >
-                            </div> 
-                    </div>   
-            
-           
-        </form>
+                            </div>
+                        </div>
+
+
+                    </form>
+                </div>
+            </div>
         </div>
-        </div>
-        </div>
-   
+
     )
 } 
